b1eedb847551cacd1d8c8692d0d5d6fd5dbb886e
CDJohnson279 <97996283+CDJohnson279@users.noreply.github.com>
2022-01-18 20:58:51 -0800

    Add files via upload

---
+++ Johnson_Dean_ECE351_Lab1.py
@@ -0,0 +1,23 @@
+#################################################################
+#                                                               #
+# Dean Johnson                                                  #
+# ECE 351                                                       #
+# Lab 1                                                         #
+# 1/25/2022                                                     #
+#                                                               #
+#################################################################
+
+import numpy
+import scipy . signal
+import time
+
+t = 1
+print ( t )
+print ("t =",t )
+print ('t =',t ," seconds")
+print('t is now =',t/3,'\n....and can be rounded using round()',round(t/3,4))
+
+print('3 squared = ',3**2,'\n')
+
+list1= [0,1,2,3]
+print('list1:',list1, '\n')
